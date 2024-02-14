b2e67cf57e9060541b90a4ef963333c162d41dee
abid211051 <c211051@ugrad.iiuc.ac.bd>
2024-02-14 22:31:00 +0600

    da

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -140,6 +140,7 @@ module.exports.ipn = async (req, res) => {
     try {
         const cartitems = await CartItem.find({ user: req.user._id })
         const profile = await Profile.findOne({ user: req.user._id });
+        console.log(req.body.status)
         if (req.body.status === 'VALID') {
             const val_id = req.body.val_id;
             const tran_id = req.body.tran_id;
